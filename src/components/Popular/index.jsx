93e8268b6d7b8189c94dede9583bc10a2443ed5e
manphamTechbee <127067970+manphamTechbee@users.noreply.github.com>
2023-05-22 18:41:49 +0700

    refactor: fix slider item gap - develop navigation button

--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,86 +1,103 @@
-import React from 'react'
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ItemSlider from '../ItemSlider';
-import './style.scss'
-import { ArrowCircleRightOutlined, ArrowCircleLeftOutlined } from '@mui/icons-material';
-function Popular({data}) {
+import ItemSlider from "../ItemSlider";
+import "./style.scss";
+import {
+  ArrowCircleRightOutlined,
+  ArrowCircleLeftOutlined,
+} from "@mui/icons-material";
+function Popular({ data }) {
+  const slideRef = useRef();
+  let slidesToShow = 4;
 
-    let slidesToShow = 5;
-
-  const PreviousBtn = (props) => {
-    const { className, onClick, currentSlide } = props;
-    return (
-      <>
-        {currentSlide !== 0 && (
-          <div className={className} onClick={onClick} style={{ position: 'absolute', left: '-17px', bottom: '-150px', zIndex: '40'}} >    
-            <ArrowCircleLeftOutlined className='arrow-icon' />
-          </div>
-
-        )}
-      </>
-    );
-  };
-  const NextBtn = (props) => {
-    const { className, onClick, slideCount, currentSlide } = props;
-    return (
-      <>
-        {currentSlide !== slideCount - slidesToShow && (
-          <div className={className} onClick={onClick}    >     
-            <ArrowCircleRightOutlined className='arrow-icon'/>
-          </div>
-        )}
-      </>
-    );
-  };
   const settings = {
-    dot: true,
     infinite: true,
     speed: 500,
+    autoplay: false,
     slidesToShow: slidesToShow,
     slidesToScroll: 1,
     initialSlide: 0,
-    nextArrow: <NextBtn />,
-    prevArrow: <PreviousBtn />,
+    pauseOnHover: true,
 
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
+      },
+      {
+        breakpoint: 797,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]      
-};
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          draggable: true,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className='info'>
-          <Slider {...settings}>
-          {data.map(item => {
-          return <ItemSlider  item={item}/>
-          })} 
-          </Slider>        
-        </div>
-  )
+    <div className="slider-container">
+      <button
+        className="sliderNavigateBtn sliderNavigateBtn--right"
+        onClick={() => {
+          slideRef.current.slickPrev();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </button>
+      <Slider {...settings} className="slider" ref={slideRef}>
+        {data.map((item) => {
+          return <ItemSlider item={item} />;
+        })}
+      </Slider>
+      <button
+        className="sliderNavigateBtn sliderNavigateBtn--next"
+        onClick={() => {
+          slideRef.current.slickNext();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
 
-export default Popular
+export default Popular;
