b0359b4d08bcc551f0e2f1f4940eb1823fa0c4a0
Kreal244 <nuunkhang@gmail.com>
2023-05-24 12:46:25 +0700

    remove chatbox, socketio

--- src/containers/ListOrders/index.jsx
+++ src/containers/ListOrders/index.jsx
@@ -1,4 +1,4 @@
-import { List } from 'antd'
+import { List, Select } from 'antd'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
@@ -6,11 +6,40 @@ import OrderItem from '../../components/OrderItem'
 import './style.scss'
 export default function ListOrders() {
   const [dataset, setDataset] = useState([])
+  const [ filter,setFilter]=useState([])
   const data = useSelector(state => state.fetchOrder.dataOrder)
   useEffect(() => {
     setDataset(data)
   }, [data])
+  const options = [{
+    lable: 'Paid',
+    value:'paid'
+  },
+    {
+      lable: 'Unpaid',
+      value: 'unpaid'
+    },
+    {
+      lable: 'Deleivered',
+      value: 'isDelivered'
+    },
+  ]
+  const handleChange = (value) => {
+    console.log(value);
+  };
   return (
+    <>
+      <div><Select
+        mode="multiple"
+        allowClear
+        style={{
+          width: '100%',
+        }}
+        placeholder="Please select"
+        defaultValue={['paid']}
+        onChange={handleChange}
+        options={options}
+      /></div>
     < List
       itemLayout="vertical"
       pagination={{
@@ -34,6 +63,7 @@ export default function ListOrders() {
         <OrderItem item={item} />
       )}
     />
+    </>
   )
 }
 
