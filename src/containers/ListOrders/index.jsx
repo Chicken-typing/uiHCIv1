d4767d0f9b690958068ddd3f36e33b1efb4cf7b2
Huynh Nguyen Khang <76195700+Kreal244@users.noreply.github.com>
2023-05-24 12:50:47 +0700

    reverse previous listorders

--- src/containers/ListOrders/index.jsx
+++ src/containers/ListOrders/index.jsx
@@ -1,4 +1,4 @@
-import { List, Select } from 'antd'
+import { List } from 'antd'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
@@ -6,40 +6,11 @@ import OrderItem from '../../components/OrderItem'
 import './style.scss'
 export default function ListOrders() {
   const [dataset, setDataset] = useState([])
-  const [ filter,setFilter]=useState([])
   const data = useSelector(state => state.fetchOrder.dataOrder)
   useEffect(() => {
     setDataset(data)
   }, [data])
-  const options = [{
-    lable: 'Paid',
-    value:'paid'
-  },
-    {
-      lable: 'Unpaid',
-      value: 'unpaid'
-    },
-    {
-      lable: 'Deleivered',
-      value: 'isDelivered'
-    },
-  ]
-  const handleChange = (value) => {
-    console.log(value);
-  };
   return (
-    <>
-      <div><Select
-        mode="multiple"
-        allowClear
-        style={{
-          width: '100%',
-        }}
-        placeholder="Please select"
-        defaultValue={['paid']}
-        onChange={handleChange}
-        options={options}
-      /></div>
     < List
       itemLayout="vertical"
       pagination={{
@@ -63,7 +34,5 @@ export default function ListOrders() {
         <OrderItem item={item} />
       )}
     />
-    </>
   )
 }
-
